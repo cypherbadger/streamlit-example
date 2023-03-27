ecd73d82278f30a97162273a0cee9eeda2555179
Cypherbadger <68619730+cypherbadger@users.noreply.github.com>
2023-03-27 18:54:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import os
 
 import streamlit as st
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from llama_index import QuestionAnswerPrompt, GPTSimpleVectorIndex, SimpleDirectoryReader
 
 # NOTE: for local testing only, do NOT deploy with your key hardcoded
 # to use this for yourself, create a file called .streamlit/secrets.toml with your api key
@@ -12,27 +12,28 @@ os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 index_name = "./index.json"
 documents_folder = "./documents"
 
+# load documents
+documents = SimpleDirectoryReader(documents_folder).load_data()
 
-@st.cache_resource
-def initialize_index(index_name, documents_folder):
-    if os.path.exists(index_name):
-        index = GPTSimpleVectorIndex.load_from_disk(index_name)
-    else:
-        documents = SimpleDirectoryReader(documents_folder).load_data()
-        index = GPTSimpleVectorIndex(documents)
-        index.save_to_disk(index_name)
+# define custom QuestionAnswerPrompt
+QA_PROMPT_TMPL = (
+    "We have provided context information below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given this information, please answer the question: {query_str}\n"
+)
+QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 
-    return index
+# Build GPTSimpleVectorIndex
+index = GPTSimpleVectorIndex(documents)
 
 
 @st.cache_data(max_entries=200, persist=True)
 def query_index(_index, query_text):
-    response = _index.query(query_text)
+    query_str = query_text
+    response = _index.query(query_str, text_qa_template=QA_PROMPT)
     return str(response)
-
-
-# This should be cached and only fully runs once
-index = initialize_index(index_name, documents_folder)
 
 
 st.title("😎🏦 BH chatbot 🏦😎")
